efd8bccc3d4db30461bc0431bb092d739d1c6617
Alpyg <alpyg@pm.me>
2023-12-19 01:04:17 -0500

    Fixed zig formatting

--- nvim/.config/nvim/lua/plugins/zig.lua
+++ nvim/.config/nvim/lua/plugins/zig.lua
@@ -25,6 +25,16 @@ return {
         "zls",
       })
     end,
-  }
-
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      if type(opts.sources) == "table" then
+        local nls = require("none-ls")
+        vim.list_extend(opts.sources, {
+          nls.builtins.formatting.zigfmt,
+        })
+      end
+    end,
+  },
 }
