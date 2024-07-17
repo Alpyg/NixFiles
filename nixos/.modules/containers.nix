ae75d82d38fb704ce8d13656d491ff9028c6e1db
Alpyg <alpyg@pm.me>
2024-07-17 18:40:40 -0400

    Fix again

--- nixos/.modules/containers.nix
+++ nixos/.modules/containers.nix
@@ -1,31 +1,30 @@
 { ... }:
 
 {
+
   virtualisation.oci-containers.backend = "docker";
   virtualisation.oci-containers.containers = {
     jellyfin = {
       image = "jellyfin/jellyfin";
-      hostname = "jellyfin";
+      hostname = "bebiflix-jellyfin";
       volumes = [
         "/home/nexus/.jellyfin/config:/config"
         "/home/nexus/.jellyfin/cache:/cache"
         "/home/nexus/.jellyfin/media:/media"
       ];
       ports = [ "8096:8096" ];
-      extraOptions = [ "--network=bebiflix" ];
       autoStart = true;
     };
     sonarr = {
       image = "lscr.io/linuxserver/sonarr:latest";
-      hostname = "sonarr";
+      hostname = "bebiflix-sonarr";
       volumes = [ "/home/nexus/.sonarr/config:/config" ];
       environment = {
-        PUID = 1000;
-        PGID = 1000;
+        PUID = "1000";
+        PGID = "1000";
         TZ = "Etc/UTC";
       };
       ports = [ "8989:8989" ];
-      extraOptions = [ "--network=bebiflix" ];
       dependsOn = [ "jellyfin" ];
       autoStart = true;
     };
