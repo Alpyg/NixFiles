2fc7509e7954f40de9c3faf78895dd62c13eb141
Alpyg <alpyg@pm.me>
2024-07-17 19:36:45 -0400

    Just use services omg

--- nixos/.modules/bebiflix.nix
+++ nixos/.modules/bebiflix.nix
@@ -2,51 +2,18 @@
 
 {
   containers.bebiflix = {
-    autoStart = true;
-    forwardPorts = [
-      {
-        containerPort = 8096;
-        hostPort = 8096;
-        protocol = "tcp";
-      }
-      {
-        containerPort = 8989;
-        hostPort = 8989;
-        protocol = "tcp";
-      }
-      {
-        containerPort = 9091;
-        hostPort = 9091;
-        protocol = "tcp";
-      }
-      {
-        containerPort = 51413;
-        hostPort = 51413;
-        protocol = "tcp";
-      }
-      {
-        containerPort = 51413;
-        hostPort = 51413;
-        protocol = "udp";
-      }
-    ];
-    config = { ... }: {
-      services.jellyfin = {
-        enable = true;
-        openFirewall = true;
-        user = "nexus";
-      };
-      services.sonarr = {
-        enable = true;
-        openFirewall = true;
-      };
-      services.transmission = {
-        enable = true;
-        openRPCPort = true;
-        settings.download-dir = "/home/nexus/Downloads";
-      };
-
-      system.stateVersion = "24.05";
+    services.jellyfin = {
+      enable = true;
+      openFirewall = true;
+      user = "nexus";
+    };
+    services.sonarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    services.transmission = {
+      enable = true;
+      openRPCPort = true;
     };
   };
 }
