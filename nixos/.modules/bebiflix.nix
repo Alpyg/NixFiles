7d901fc92cea5a64ce3519d17e39c42cee2a3117
Alpyg <alpyg@pm.me>
2024-07-17 19:37:14 -0400

    Properly

--- nixos/.modules/bebiflix.nix
+++ nixos/.modules/bebiflix.nix
@@ -1,19 +1,17 @@
 { ... }:
 
 {
-  containers.bebiflix = {
-    services.jellyfin = {
-      enable = true;
-      openFirewall = true;
-      user = "nexus";
-    };
-    services.sonarr = {
-      enable = true;
-      openFirewall = true;
-    };
-    services.transmission = {
-      enable = true;
-      openRPCPort = true;
-    };
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "nexus";
+  };
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.transmission = {
+    enable = true;
+    openRPCPort = true;
   };
 }
