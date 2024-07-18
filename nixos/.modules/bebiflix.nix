97d124175509d6077d68b864151e5b6058f6ffbc
Alpyg <alpyg@pm.me>
2024-07-17 20:10:37 -0400

    Readded paths

--- nixos/.modules/bebiflix.nix
+++ nixos/.modules/bebiflix.nix
@@ -5,15 +5,16 @@
     enable = true;
     openFirewall = true;
     user = "nexus";
+    dataDir = "/home/nexus/.jellyfin";
   };
   services.sonarr = {
     enable = true;
     openFirewall = true;
     user = "nexus";
+    dataDir = "/home/nexus/.sonarr";
   };
   services.transmission = {
     enable = true;
     openFirewall = true;
-    user = "nexus";
   };
 }
