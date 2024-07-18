8149493e8771d7324d1df295bc320efbb2acc0cd
Alpyg <alpyg@pm.me>
2024-07-17 20:04:09 -0400

    Reset paths and added user to transmissino

--- nixos/.modules/bebiflix.nix
+++ nixos/.modules/bebiflix.nix
@@ -5,16 +5,15 @@
     enable = true;
     openFirewall = true;
     user = "nexus";
-    dataDir = "/home/nexus/.jellyfin";
   };
   services.sonarr = {
     enable = true;
     openFirewall = true;
     user = "nexus";
-    dataDir = "/home/nexus/.sonarr";
   };
   services.transmission = {
     enable = true;
     openFirewall = true;
+    user = "nexus";
   };
 }
