ed06dcbb852405e141da676131f289e11f65c90d
Alpyg <alpyg@pm.me>
2024-07-17 20:00:50 -0400

    Fix transmission port

--- nixos/.modules/bebiflix.nix
+++ nixos/.modules/bebiflix.nix
@@ -5,16 +5,16 @@
     enable = true;
     openFirewall = true;
     user = "nexus";
-    dataDir = "/home/nexus/jellyfin";
+    dataDir = "/home/nexus/.jellyfin";
   };
   services.sonarr = {
     enable = true;
     openFirewall = true;
     user = "nexus";
-    dataDir = "/home/nexus/jellyfin";
+    dataDir = "/home/nexus/.sonarr";
   };
   services.transmission = {
     enable = true;
-    openRPCPort = true;
+    openFirewall = true;
   };
 }
