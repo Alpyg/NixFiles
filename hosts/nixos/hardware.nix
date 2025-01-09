a8866833b642391ed4df050e746465167307e82a
Alpyg <alpyg@pm.me>
2025-01-09 07:30:46 -0500

    nvme

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -6,24 +6,45 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/4de1a9c0-f3f4-4241-ac17-660b3ae851bd";
-    fsType = "ext4";
-  };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/577323f9-209b-4c10-a447-aea9af34a6cf";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/577323f9-209b-4c10-a447-aea9af34a6cf";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/3A26-0A4F";
-    fsType = "vfat";
-  };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/577323f9-209b-4c10-a447-aea9af34a6cf";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
 
-  fileSystems."/mnt/x" = {
-    device = "/dev/disk/by-uuid/d7a408d4-44e1-4717-9385-494f15784a58";
-    fsType = "ext4";
-  };
+    "/var" = {
+      device = "/dev/disk/by-uuid/577323f9-209b-4c10-a447-aea9af34a6cf";
+      fsType = "btrfs";
+      options = [ "subvol=var" ];
+    };
 
-  fileSystems."/mnt/y" = {
-    device = "/dev/disk/by-uuid/bf9b64e3-b69d-4f00-8201-1b7237934366";
-    fsType = "ext4";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/0C00-9FD9";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+    "/mnt/x" = {
+      device = "/dev/disk/by-uuid/d7a408d4-44e1-4717-9385-494f15784a58";
+      fsType = "ext4";
+    };
+
+    "/mnt/y" = {
+      device = "/dev/disk/by-uuid/bf9b64e3-b69d-4f00-8201-1b7237934366";
+      fsType = "ext4";
+    };
   };
 
   swapDevices = [{
